d41287ea523d177970935d7fc0082feb25b74e8c
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-07-09 15:51:46 +0200

    better styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import '../globals.css';
 </script>
 
-<div class="flex h-screen w-full flex-col bg-primary dark:bg-primary_dark">
+<div>
     <slot />
 </div>
--- src/routes/_/search/+page.svelte
+++ src/routes/_/search/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
-    export let data
+    export let data;
 </script>
 
-{#each data.objects as d}
-<div class="flex flex-col h-24 rounded-xl border-2 border-black bg-primary p-2 mx-2 mt-2 text-primary_dark dark:border-white dark:bg-primary_dark dark:text-primary">
-    <span>{d.package.name}</span>
-    <span>{d.package.author?.username || d.package.author?.name || ''}</span>
-</div>
+<svelte:head>
+    <title>npkg - results for</title>
+</svelte:head>
+
+{#each data.objects as o}
+    <a href={o.package.links.npm}>
+        <div
+            class="m-2 flex h-24 flex-col rounded-xl border-2 border-black bg-primary p-2 text-primary_dark dark:border-white dark:bg-primary_dark dark:text-primary"
+        >
+            <span class="font-extrabold">{o.package.name}</span>
+            <span>{o.package.description}</span>
+            <div class="pt-1">
+                <span
+                    class="w-fit rounded-xl border-2 border-black p-1 dark:border-white"
+                    >{o.package.version}
+                    <span
+                        class="w-fit rounded-xl border-2 border-black px-1 dark:border-white"
+                        >{new Date(o.package.date).toLocaleDateString()}</span
+                    ></span
+                >
+            </div>
+        </div>
+    </a>
 {/each}
