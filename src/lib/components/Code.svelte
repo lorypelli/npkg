d542da42aebeb9ce2c4a8b817a6306d079b4df4c
lorypelli <lorypelli@proton.me>
2024-08-10 23:27:25 +0200

    continue working on select menu

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -7,8 +7,10 @@
 
 <span class="text-center text-xl">Code for {pkg}</span>
 <span class="text-center text-md font-bold">{version}</span>
-<select class="mx-2 rounded-md p-1">
-    <option>Select a File</option>
+<select class="mx-2 rounded-md p-1" on:change={(e) => {
+    location.href = `/${pkg}/v/${version}/code/${e.currentTarget.value}`;
+}}>
+    <option hidden selected value>Select a File</option>
     {#each Object.entries(code) as [k, v]}
         <option value={v.hex}>{k.slice(1)}</option>
     {/each}
