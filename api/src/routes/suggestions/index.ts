6d1f702fa92f85f4ce1fd57161513282b75f40cb
lorypelli <lorypelli@proton.me>
2024-08-15 17:49:22 +0200

    fix `internal server error`

--- api/src/routes/suggestions/index.ts
+++ api/src/routes/suggestions/index.ts
@@ -10,17 +10,22 @@ export default async function suggestions(ctx: Context) {
     const suggestions = await fetch(
         `https://www.npmjs.com/search/suggestions?q=${q}`,
     );
+    if (!suggestions.ok) {
+        return ctx.notFound();
+    }
     const pkg_suggestions: Search['packages'] = await suggestions.json();
     const packages: Search['packages'] = [];
     for (let i = 0; i < parseInt(size); i++) {
-        packages.push({
-            date: pkg_suggestions[i].date,
-            description: pkg_suggestions[i].description || '',
-            keywords: pkg_suggestions[i].keywords || [],
-            name: pkg_suggestions[i].name,
-            publisher: pkg_suggestions[i].publisher,
-            version: pkg_suggestions[i].version,
-        });
+        if (pkg_suggestions[i]) {
+            packages.push({
+                date: pkg_suggestions[i].date,
+                description: pkg_suggestions[i].description || '',
+                keywords: pkg_suggestions[i].keywords || [],
+                name: pkg_suggestions[i].name,
+                publisher: pkg_suggestions[i].publisher,
+                version: pkg_suggestions[i].version,
+            });
+        }
     }
     return ctx.json(packages);
 }
