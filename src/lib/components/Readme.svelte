df18be7c5b180c9c47472ba410e097077b3087d9
lorypelli <lorypelli@proton.me>
2024-08-17 15:26:33 +0200

    fix readme

--- src/lib/components/Readme.svelte
+++ src/lib/components/Readme.svelte
@@ -3,10 +3,10 @@
     export let version: string;
 </script>
 
-<div class="flex w-full items-center justify-center md:w-1/2">
+<div class="flex h-full w-full items-center justify-center md:w-1/2">
     <iframe
         src="/{name}/v/{version}/readme"
-        width="1150px"
+        width="100%"
         height="100%"
         title="Readme"
         sandbox="allow-popups allow-popups-to-escape-sandbox"
