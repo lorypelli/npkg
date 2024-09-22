a96a7666b03a5a7541ec4a1b577c0e7a0b28411e
lorypelli <lorypelli@proton.me>
2024-09-22 16:28:06 +0200

    added `cdn`

---
+++ cdn/src/index.ts
@@ -0,0 +1,18 @@
+import md5 from 'crypto-js/md5';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', async (ctx) => {
+    const email = ctx.req.query('email');
+    const emailHash = email ? md5(email) : '';
+    const res = await fetch(
+        `https://s.gravatar.com/avatar/${emailHash}?default=retro`,
+    );
+    if (!res.ok) {
+        return ctx.json({ error: res.statusText }, 500);
+    }
+    return ctx.body(await res.arrayBuffer());
+});
+
+export default app;
--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,18 +1,9 @@
 <script lang="ts">
-    import md5 from 'crypto-js/md5';
     export let email = '';
 </script>
 
-{#if email}
-    <img
-        class="size-6"
-        src="https://s.gravatar.com/avatar/{md5(email)}?default=retro"
-        alt="pic"
-    />
-{:else}
-    <img
-        class="size-6"
-        src="https://s.gravatar.com/avatar/?default=retro"
-        alt="pic"
-    />
-{/if}
+<img
+    class="size-6"
+    src="https://cdn.npkg.lorypelli.dev/?email={email}"
+    alt="pic"
+/>
