9a37149adad4a813a7682e1d85a5da313dc13f71
lorypelli <lorypelli@proton.me>
2024-10-11 20:30:40 +0200

    allow file to be retrieved even without `hex`

--- src/routes/(pkg)/[...pkg]/v/[v]/code/[...f]/+page.server.ts
+++ src/routes/(pkg)/[...pkg]/v/[v]/code/[...f]/+page.server.ts
@@ -1,11 +1,25 @@
 import { BASE_URL } from '$lib/utils/url.ts';
 import { error } from '@sveltejs/kit';
-import type { File } from '../../../../../../../../types/package.ts';
+import type { File, NPMCode } from '../../../../../../../../types/package.ts';
 
 export async function load({ params: { pkg, v, f }, url }) {
-    const hex = url.searchParams.get('hex');
-    if (!pkg || !v || !f || !hex) {
+    let hex = url.searchParams.get('hex');
+    if (!pkg || !v || !f) {
         error(404);
+    }
+    if (!hex) {
+        const code = await fetch(
+            `https://www.npmjs.com/package/${pkg}/v/${v}/index`,
+        );
+        if (!code.ok) {
+            error(404);
+        }
+        const pkg_code: NPMCode = await code.json();
+        for (let file in pkg_code.files) {
+            if (file.slice(1) == f) {
+                hex = pkg_code.files[file].hex;
+            }
+        }
     }
     const file = await fetch(
         `${BASE_URL}/pkg/${encodeURIComponent(pkg)}/${v}/code/${hex}`,
