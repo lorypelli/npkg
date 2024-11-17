451e98594d73a541565b932ac782b08053e3acba
lorypelli <lorypelli@proton.me>
2024-11-17 12:38:09 +0100

    fix again

--- src/routes/(pkg)/+layout.svelte
+++ src/routes/(pkg)/+layout.svelte
@@ -7,22 +7,6 @@
     let { children }: Props = $props();
 </script>
 
-<svelte:head>
-    <title>npkg: fast and efficient alternative to the slow npm website!</title>
-    <meta
-        name="og:title"
-        content="npkg - fast and efficient alternative to the slow npm website!"
-    />
-    <meta
-        name="description"
-        content="NPKG is a fast and efficient alternative to the slow npm website. If you're tired of sluggish page loads and inefficient browsing on npm's official site, npkg is here to offer you a lightning-fast experience for exploring packages, managing dependencies, and copying useful scripts."
-    />
-    <meta
-        name="og:description"
-        content="NPKG is a fast and efficient alternative to the slow npm website. If you're tired of sluggish page loads and inefficient browsing on npm's official site, npkg is here to offer you a lightning-fast experience for exploring packages, managing dependencies, and copying useful scripts."
-    />
-</svelte:head>
-
 <nav class="flex justify-center p-2 sm:justify-between">
     <div class="hidden sm:block">
         <a href="/">
