95f6ef0ddaf95cfff9097e13f05b9317127fc5e4
lorypelli <lorypelli@proton.me>
2025-06-25 11:48:12 +0200

    fix if check

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,8 +2,8 @@ export function reroute({ url: { pathname, searchParams } }) {
     const paths = pathname.split('/').filter((v) => v != '');
     if (
         !searchParams.has('q') &&
-        (paths.length == 1 ||
-            (paths.length > 0 && paths.length < 3 && paths[0].startsWith('@')))
+        paths.length > 0 &&
+        (paths.length == 1 || (paths.length == 2 && paths[0].startsWith('@')))
     ) {
         return `/${paths.join('/')}/v/latest`;
     }
