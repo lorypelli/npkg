30212a698dc97693d337082eed44335bf7a7141c
lorypelli <lorypelli@proton.me>
2025-06-25 09:36:35 +0200

    fix team packages

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,11 @@
 export function reroute({ url: { pathname, searchParams } }) {
     const paths = pathname.split('/').filter((v) => v != '');
-    if (paths.length == 1 && !searchParams.has('q')) {
-        return `/${paths[0]}/v/latest`;
+    if (
+        !searchParams.has('q') &&
+        (paths.length == 1 ||
+            (paths.length > 0 && paths.length < 3 && paths[0].startsWith('@')))
+    ) {
+        return `/${paths.join('/')}/v/latest`;
     }
     return pathname;
 }
