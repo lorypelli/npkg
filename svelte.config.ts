04571be20ef6bf4d7de46451c267cf37f7c8b16c
lorypelli <lorypelli@proton.me>
2025-07-03 08:06:17 +0200

    migrate to `.ts`

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,10 +1,10 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 export default {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter(),
     },
-};
+} satisfies Config;
