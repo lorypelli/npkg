33236a4729a0c2a2a8ebdbbf19e8ff5b25db5d01
lorypelli <lorypelli@proton.me>
2025-07-13 10:00:31 +0200

    added `Content-Type`

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -8,13 +8,13 @@
 {#if email}
     <img
         class="size-8 rounded-full border-2 border-black dark:border-white"
-        src="https://cdn.npkg.lorypelli.dev/?email={email}"
+        src="https://cdn.npkg.lorypelli.dev/?email={email}&size=32"
         alt="pic"
     />
 {:else}
     <img
         class="size-8 rounded-full border-2 border-black dark:border-white"
-        src="https://cdn.npkg.lorypelli.dev/"
+        src="https://cdn.npkg.lorypelli.dev/?size=32"
         alt="pic"
     />
 {/if}
